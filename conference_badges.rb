66eee2c2d3c091e62fff8646a5c318462d16de4b
Bernadette Davis <bsd231@nyu.edu>
2019-05-15 14:15:53 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,36 @@
 # Write your code here.
+
+
+
+def badge_maker(inputs)
+    return "Hello, my name is #{name}."
+
+end
+
+def batch_badge_creator(attendees)
+  $badges = []
+
+  attendees.each {|attendee|
+    $badges << "Hello, my name is #{attendee}."
+  }
+
+  $badges
+end
+
+def assign_rooms(attendees)
+  $room_assignments = []
+  roomNumber = 1
+
+  attendees.each {|attendee|
+    $room_assignments << "Hello, #{attendee}! You'll be assigned to room #{roomNumber}!"
+    roomNumber += 1
+  }
+  $room_assignments
+
+end
+
+def printer(attendees)
+  attendees.each {|attendee|
+    puts "Hello, my name is #{attendee}."
+  }
+end
